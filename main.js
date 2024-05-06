a4640c4a7e14e7857771038b23046bd8b3891e59
Marco <marcosantos1671465@gmail.com>
2024-05-06 15:59:00 -0300

    Parte visual desktop finalizada

---
+++ main.js
@@ -0,0 +1,9 @@
+email = document.getElementById('email');
+form = document.getElementById('emailForm');
+
+form.addEventListener('submit', (e) =>{
+    let messages = [];
+
+
+    e.preventDefault();
+})
