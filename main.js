55bfd6739b4fcdc42fce8adba6754d0778be5aae
Marco <marcosantos1671465@gmail.com>
2024-05-07 15:53:36 -0300

    validação de formulário parcialmente concluída

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-email = document.getElementById('email');
-form = document.getElementById('emailForm');
+let emailInput = document.getElementById('email');
+let submitBtn = document.getElementsByClassName('btn')[0];
 
-form.addEventListener('submit', (e) =>{
-    let messages = [];
+const errorMsg = document.getElementById('error');
 
+submitBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
 
-    e.preventDefault();
+    const emailValue = emailInput.value;
+    emailValue.trim();
+    if(emailValue == ''){
+        errorMsg.innerText = "Oops! please check your email" ;
+
+        setTimeout(() => {
+            errorMsg.innerText = '';
+        }, 3000);
+
+        return;
+    }  
 })
+
