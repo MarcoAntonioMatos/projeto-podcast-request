93b53c6e94d60cb3101061be020a75bca2193722
Marco <marcosantos1671465@gmail.com>
2024-05-13 15:33:43 -0300

    Versão  Desktop concluído

--- main.js
+++ main.js
@@ -8,14 +8,13 @@ submitBtn.addEventListener('click', (e)=>{
 
     const emailValue = emailInput.value;
     emailValue.trim();
-    if(emailValue == ''){
+    if(emailValue == '' || !emailValue.match(/^[A-Za-z\._\-0-9]+@[A-Za-z\.\-]+\.[a-z]{2,4}$/)){
         errorMsg.innerText = "Oops! please check your email" ;
 
         setTimeout(() => {
             errorMsg.innerText = '';
         }, 3000);
-
-        return;
+        return ;
     }  
 })
 
